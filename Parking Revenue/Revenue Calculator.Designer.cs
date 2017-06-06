0efcb8931a82d18ea266ef0e5d0cda58c28ec8f3
spenc <spenc@LAPTOP-8LL6OQLE>
2017-06-06 11:48:10 -0600

    Add project files.

---
+++ Parking Revenue/Revenue Calculator.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Parking_Revenue
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.southCommuterTextBox = new System.Windows.Forms.TextBox();
+            this.eastCommuterTextBox = new System.Windows.Forms.TextBox();
+            this.westCommuterTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.revenueGroup = new System.Windows.Forms.GroupBox();
+            this.tRevenueLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.sRevenueLabel = new System.Windows.Forms.Label();
+            this.eRevenueLabel = new System.Windows.Forms.Label();
+            this.wRevenueLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calculateRevButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.revenueGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.southCommuterTextBox);
+            this.groupBox1.Controls.Add(this.eastCommuterTextBox);
+            this.groupBox1.Controls.Add(this.westCommuterTextBox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 178);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Permits Sold";
+            // 
+            // southCommuterTextBox
+            // 
+            this.southCommuterTextBox.Location = new System.Drawing.Point(99, 142);
+            this.southCommuterTextBox.Name = "southCommuterTextBox";
+            this.southCommuterTextBox.Size = new System.Drawing.Size(84, 20);
+            this.southCommuterTextBox.TabIndex = 6;
+            // 
+            // eastCommuterTextBox
+            // 
+            this.eastCommuterTextBox.Location = new System.Drawing.Point(99, 111);
+            this.eastCommuterTextBox.Name = "eastCommuterTextBox";
+            this.eastCommuterTextBox.Size = new System.Drawing.Size(84, 20);
+            this.eastCommuterTextBox.TabIndex = 5;
+            // 
+            // westCommuterTextBox
+            // 
+            this.westCommuterTextBox.Location = new System.Drawing.Point(99, 81);
+            this.westCommuterTextBox.Name = "westCommuterTextBox";
+            this.westCommuterTextBox.Size = new System.Drawing.Size(84, 20);
+            this.westCommuterTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "South Commuter:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "East Commuter:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "West Commuter:";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the number of permits sold for each type of permit";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // revenueGroup
+            // 
+            this.revenueGroup.Controls.Add(this.tRevenueLabel);
+            this.revenueGroup.Controls.Add(this.label8);
+            this.revenueGroup.Controls.Add(this.sRevenueLabel);
+            this.revenueGroup.Controls.Add(this.eRevenueLabel);
+            this.revenueGroup.Controls.Add(this.wRevenueLabel);
+            this.revenueGroup.Controls.Add(this.label5);
+            this.revenueGroup.Controls.Add(this.label6);
+            this.revenueGroup.Controls.Add(this.label7);
+            this.revenueGroup.Location = new System.Drawing.Point(277, 25);
+            this.revenueGroup.Name = "revenueGroup";
+            this.revenueGroup.Size = new System.Drawing.Size(200, 178);
+            this.revenueGroup.TabIndex = 1;
+            this.revenueGroup.TabStop = false;
+            this.revenueGroup.Text = "Revenue Generated";
+            // 
+            // tRevenueLabel
+            // 
+            this.tRevenueLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tRevenueLabel.Location = new System.Drawing.Point(97, 149);
+            this.tRevenueLabel.Name = "tRevenueLabel";
+            this.tRevenueLabel.Size = new System.Drawing.Size(80, 18);
+            this.tRevenueLabel.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 13);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Total Revenue:";
+            // 
+            // sRevenueLabel
+            // 
+            this.sRevenueLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.sRevenueLabel.Location = new System.Drawing.Point(97, 104);
+            this.sRevenueLabel.Name = "sRevenueLabel";
+            this.sRevenueLabel.Size = new System.Drawing.Size(80, 18);
+            this.sRevenueLabel.TabIndex = 9;
+            // 
+            // eRevenueLabel
+            // 
+            this.eRevenueLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.eRevenueLabel.Location = new System.Drawing.Point(97, 73);
+            this.eRevenueLabel.Name = "eRevenueLabel";
+            this.eRevenueLabel.Size = new System.Drawing.Size(80, 18);
+            this.eRevenueLabel.TabIndex = 8;
+            // 
+            // wRevenueLabel
+            // 
+            this.wRevenueLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.wRevenueLabel.Location = new System.Drawing.Point(97, 43);
+            this.wRevenueLabel.Name = "wRevenueLabel";
+            this.wRevenueLabel.Size = new System.Drawing.Size(80, 18);
+            this.wRevenueLabel.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "South Commuter:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "East Commuter:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 43);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "West Commuter:";
+            // 
+            // calculateRevButton
+            // 
+            this.calculateRevButton.Location = new System.Drawing.Point(69, 244);
+            this.calculateRevButton.Name = "calculateRevButton";
+            this.calculateRevButton.Size = new System.Drawing.Size(104, 48);
+            this.calculateRevButton.TabIndex = 2;
+            this.calculateRevButton.Text = "Calculate Revenue";
+            this.calculateRevButton.UseVisualStyleBackColor = true;
+            this.calculateRevButton.Click += new System.EventHandler(this.calculateRevButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(197, 244);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(104, 48);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(324, 244);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(104, 48);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 323);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateRevButton);
+            this.Controls.Add(this.revenueGroup);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Permit Revenue Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.revenueGroup.ResumeLayout(false);
+            this.revenueGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox southCommuterTextBox;
+        private System.Windows.Forms.TextBox eastCommuterTextBox;
+        private System.Windows.Forms.TextBox westCommuterTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox revenueGroup;
+        private System.Windows.Forms.Label tRevenueLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label sRevenueLabel;
+        private System.Windows.Forms.Label eRevenueLabel;
+        private System.Windows.Forms.Label wRevenueLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button calculateRevButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Parking Revenue/Revenue Calculator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ Spencer Henze
+ ITM 225
+ Kit Scott
+ 9/12/16
+ */
+
+namespace Parking_Revenue
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void calculateRevButton_Click(object sender, EventArgs e)
+        {
+            try // this tries to run this code by default, but if the user enters something that makes this not work, run the catch code below
+            {  //Declare all variables we'll need to use. Use decimals for all because we are dealing with money ultimately.
+                decimal wPermits, ePermits, sPermits;
+                decimal wRevenue, eRevenue, sRevenue, tRevenue;
+                //Declare all constant values: Prices
+                const decimal wCPrice = 140m;
+                const decimal eCPrice = 118m;
+                const decimal sCPrice = 118m;
+
+                //When the user enters the number it will be a string. Need to convert it to decimal to use it in calculations. Use the Parse method.
+                wPermits = decimal.Parse(westCommuterTextBox.Text);
+                ePermits = decimal.Parse(eastCommuterTextBox.Text);
+                sPermits = decimal.Parse(southCommuterTextBox.Text);
+
+                // Assign the variables to the math equations that define them.
+                wRevenue = wPermits * wCPrice;
+                eRevenue = ePermits * eCPrice;
+                sRevenue = sPermits * sCPrice;
+                tRevenue = wRevenue + eRevenue + sRevenue;
+
+                // To display the numbers in the label boxes, we need to convert them back to the string data type. Ust the ToString method and specify currency formatting by putting ' "C" ' in as a perameter for To.String
+                wRevenueLabel.Text = wRevenue.ToString("C");
+                eRevenueLabel.Text = eRevenue.ToString("C");
+                sRevenueLabel.Text = sRevenue.ToString("C");
+                tRevenueLabel.Text = tRevenue.ToString("C");
+            }
+            catch // Runs this code to show an error box if the user enters an incompatible data.
+            {
+                MessageBox.Show("Please enter numeric values.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        } //End Click Calculate Revenue Button
+     
+
+        private void clearButton_Click(object sender, EventArgs e) // specify what the program should do when the "Clear" button is clicked
+        { // set all labels and text fields to empty, denoted by "" 
+            westCommuterTextBox.Text = "";
+            eastCommuterTextBox.Text = "";
+            southCommuterTextBox.Text = ""; 
+
+            wRevenueLabel.Text = "";
+            eRevenueLabel.Text = "";
+            sRevenueLabel.Text = "";
+            tRevenueLabel.Text = "";
+        } //End Clear Button Click
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close(); //Close the program when the Exit button is clicked.
+        } // End Exit Button Click
+    }
+}
